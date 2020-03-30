2263d5321697db45969caa7242e816ae9f358d19
adrian-amigues <adrian.amigues@gmail.com>
2020-03-30 12:20:38 +0200

    add SimilarGames query

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,5 +1,4 @@
 const axios = require('axios')
-const parseStringPromise = require('xml2js').parseStringPromise
 
 module.exports = {
   Query: {
@@ -8,15 +7,28 @@ module.exports = {
         .get(
           'https://api.geekdo.com/api/hotness?geeksite=boardgame&nosession=1&objecttype=thing&showcount=30'
         )
-        .then(({ data }) => data.items.splice(0, 5))
+        .then(({ data }) => data.items.splice(0, 10))
         .catch(error => {
           console.log('error hotGames: ', error)
+        })
+    },
+    similarGames: (_, args) => {
+      return axios
+        .get(
+          `https://api.geekdo.com/api/geekitem/recs?ajax=1&objectid=${args.id}&objecttype=thing&pageid=1`
+        )
+        .then(({ data }) => {
+          return data.recs.map(rec => ({ ...rec.item.item }))
+        })
+        .catch(error => {
+          console.log('error similarGames: ', error)
         })
     },
   },
   HotGame: {
     id: parent => parent.objectid,
-    name: parent => parent.name,
+    bggHref: parent => `https://boardgamegeek.com${parent.href}`,
+    yearPublished: parent => parent.yearpublished,
     thumbnailUrl: parent => parent.squareimageurl,
     imageUrl: ({ objectid }, _, { dataSources }) => {
       return dataSources.xmlAPI2
@@ -28,5 +40,24 @@ module.exports = {
         .getGame(objectid)
         .then(result => result.description[0])
     },
+    minPlayers: ({ objectid }, _, { dataSources }) => {
+      return dataSources.xmlAPI2
+        .getGame(objectid)
+        .then(result => result.minplayers[0].$.value)
+    },
+    maxPlayers: ({ objectid }, _, { dataSources }) => {
+      return dataSources.xmlAPI2
+        .getGame(objectid)
+        .then(result => result.maxplayers[0].$.value)
+    },
+  },
+  SimilarGame: {
+    id: parent => parent.primaryname.nameid,
+    bggHref: parent => `https://boardgamegeek.com${parent.href}`,
+    name: parent => parent.primaryname.name,
+    imageUrl: parent => parent.images.previewthumb,
+    rank: parent => parent.dynamicinfo.item.rankinfo[0].rank,
+    rating: parent => parent.dynamicinfo.item.rankinfo[0].baverage,
+    weight: parent => parent.dynamicinfo.item.stats.avgweight,
   },
 }
--- src/typeDefs/index.js
+++ src/typeDefs/index.js
@@ -2,16 +2,30 @@ const { gql } = require('apollo-server')
 
 module.exports = gql`
   type HotGame {
-    id: ID
-    name: String
+    id: ID!
+    bggHref: String
+    yearPublished: Int
     thumbnailUrl: String
+    name: String
     imageUrl: String
     rank: Int
-    yearpublished: Int
     description: String
+    minPlayers: Int
+    maxPlayers: Int
+  }
+
+  type SimilarGame {
+    id: ID!
+    bggHref: String
+    name: String
+    imageUrl: String
+    rank: Int
+    rating: Float
+    weight: Float
   }
 
   type Query {
     hotGames: [HotGame]
+    similarGames(id: ID!): [SimilarGame]
   }
 `
